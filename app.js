6963b008eeab58274898999cf9f757ac0c44d71d
Hanback Choi <saeree@ajou.ac.kr>
2025-11-25 18:21:24 +0900

    Add basic express server

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/user/:id', (req, res) => {
+    res.send(`User id is ${req.params.id}`);
+})
+
+app.get('/user', (req, res) => {
+    res.send(`User id is ${req.query.id}`);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
