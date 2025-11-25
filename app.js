9afd562db248561f85ca7fe3a7780defd0d767c5
Hanback Choi <saeree@ajou.ac.kr>
2025-11-25 18:36:15 +0900

    add morgan logger and POST

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+const logger = require('morgan');
+
+// npm install morgan --save 필요
+var app = express()
 const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}));
+app.use(logger('dev'));
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -14,7 +22,13 @@ app.get('/user', (req, res) => {
     res.send(`User id is ${req.query.id}`);
 })
 
+app.post('/user', (req, res) => {
+    console.log(req.body.name);
+    res.send(req.body);
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
+ 
